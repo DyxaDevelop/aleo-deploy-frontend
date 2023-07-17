31bdd7b1239bcb656c5b33ff7fb0c5cd9bd05048
Andrii <iwanoa.nick2010@gmail.com>
2023-07-17 13:19:38 +0200

    blur live feed

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-const ModalWrap = styled.div(({ theme }) => ({
+const ModalWrap = styled.div(({}) => ({
   position: 'fixed',
   width: '100%',
   height: '100%',
